94ef0229a1cc0b7d281d2e21a0a4a6791c0d9f13
Marco Mercado Lugo <marcomercadolugo@customer-DGO-28-64.megared.net.mx>
2018-08-12 22:02:28 -0500

    server changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cotizadorwebadmin'));
+app.use(express.static(__dirname + '/dist/cotizador-web-admin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/cotizadorwebadmin/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cotizador-web-admin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
