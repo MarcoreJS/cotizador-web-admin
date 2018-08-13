5676bd16960fa08c925111f664f7ebd041f7e729
Marco Mercado Lugo <marcomercadolugo@customer-DGO-28-64.megared.net.mx>
2018-08-12 21:40:29 -0500

    express server y heroku deploy

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   error: any;
+  email: any;
+  password: any;
 
   constructor(
     private afa: AngularFireAuth,
