3691c9f3f2b931d2e229aebf765e1950021f6b56
Marco Mercado Lugo <marcomercadolugo@customer-DGO-28-64.megared.net.mx>
2018-08-12 21:36:58 -0500

    deploy heroku 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBgKkbb2FtKwh1WHaruA79-NM8V23keNJI",
+    authDomain: "pruebas-micod.firebaseapp.com",
+    databaseURL: "https://pruebas-micod.firebaseio.com",
+    projectId: "pruebas-micod",
+    storageBucket: "pruebas-micod.appspot.com",
+    messagingSenderId: "364132234406"
+  }
 };
